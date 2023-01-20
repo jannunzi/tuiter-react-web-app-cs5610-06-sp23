5c29d926c3a7653adadf92feeffee252c4e1fe08
JoseAnnunziatoSonos <jose.annunziato@sonos.com>
2023-01-20 08:30:50 -0500

    a2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const a = 123
+  const b = 234
+  let c = a + b
+  const d = [a, b, c]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Web Dev CS5610 Section 6.
+        </p>
+        <p>
+          a = {a}
+          <br/>
+          b = {b}
+        <br/>
+          c = {c}
+          <br/>
+          d = {d}
+          <ul>
+            <li>{d[0]}</li>
+            <li>{d[1]}</li>
+            <li>{d[2]}</li>
+          </ul>
+          <ol>
+            {d.map(item => <li>{item}</li>)}
+          </ol>
         </p>
         <a
           className="App-link"
